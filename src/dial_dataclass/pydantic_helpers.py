76f4fe4367e3cd9c96f98aaa762bf76d02326e1f
Lance-Drane <Lance-Drane@users.noreply.github.com>
2025-05-12 18:43:48 -0400

    fix BSON and pymongo import conflict
    Signed-off-by: Lance-Drane <Lance-Drane@users.noreply.github.com>

--- src/dial_dataclass/pydantic_helpers.py
+++ src/dial_dataclass/pydantic_helpers.py
@@ -1,7 +1,32 @@
 from typing import Annotated, Any
 
-from bson import ObjectId
-from bson.errors import InvalidId
+try:
+    from bson import ObjectId
+    from bson.errors import InvalidId
+except ImportError:
+    # limited fixtures for Clients to use so they don't have to install pymongo, not for use by the Service
+    class InvalidId(Exception):  # type: ignore[no-redef] # noqa: N818
+        pass
+
+    class ObjectId:  # type: ignore[no-redef]
+        def __init__(self, oid: str):
+            # this is a very limited constructor which assumes that any ObjectID instances will be created by the Service, which will not use this class
+            if len(oid) != 24:
+                raise InvalidId
+            # enforce string with 24 hex characters, catch exception in
+            bytes.fromhex(oid)
+            self._id = oid
+
+        def __str__(self):
+            return self._id
+
+        def __repr__(self):
+            return f"ObjectId('{self!s}')"
+
+        def __hash__(self):
+            return hash(self._id)
+
+
 from pydantic_core import core_schema
 
 
@@ -17,7 +42,7 @@ class _ObjectIdPydanticAnnotation:
         s = handler(v)
         try:
             return ObjectId(s)
-        except (InvalidId, TypeError) as e:
+        except (InvalidId, TypeError, ValueError) as e:
             msg = 'Invalid ObjectId - use the 24-character hex representation'
             raise ValueError(msg) from e
 
@@ -46,9 +71,15 @@ ValidatedObjectId = Annotated[
     ObjectId,
     _ObjectIdPydanticAnnotation,
 ]
-"""This is the actual annotation you can use on a BaseModel property.
+"""
+Basic representation of the DIAL id structure, useful for double-checking Client code.
 
-By validating this through Pydantic, INTERSECT knows that it can tell clients directly that their workflow_id is in an invalid format.
+## Usage
 
-(We explicitly need to raise validation errors through Pydantic if we want clients to understand the error, raising generic Exceptions means that INTERSECT will not reveal the reason for failure in the error message)
+If you are a Client, you don't actually need to import this class. You can initialize associated properties with a simple string or an existing variable, like this:
+
+```
+valid_hex_string = 'deadbeef12345678deadbeef'
+value = DialWorkflowDatasetUpdate(workflow_id=valid_hex_string)
+```
 """
