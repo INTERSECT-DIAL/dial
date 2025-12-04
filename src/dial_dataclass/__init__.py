89c03899c12e8439ddd7f9ea74d82ca1bc4e1395
Lance-Drane <Lance-Drane@users.noreply.github.com>
2025-12-04 14:22:02 -0500

    return original workflow ID in all responses
    Signed-off-by: Lance-Drane <Lance-Drane@users.noreply.github.com>

--- src/dial_dataclass/__init__.py
+++ src/dial_dataclass/__init__.py
@@ -7,4 +7,8 @@ from .dial_dataclass import (
     DialWorkflowCreationParamsClient,
     DialWorkflowDatasetUpdate,
 )
+from .dial_dataclass_responses import (
+    DialDataResponse1D,
+    DialDataResponse2D,
+)
 from .pydantic_helpers import ValidatedObjectId
