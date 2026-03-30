b904b3496e10f2eb05c5e8b30055085f92da548a
Ankit Shrivastava <shrivastavaa@ornl.gov>
2026-03-30 13:08:12 -0400

    making sinuoidal work

--- scripts/1d_sinusoidal_growth_client.py
+++ scripts/1d_sinusoidal_growth_client.py
@@ -94,6 +94,7 @@ class ActiveLearningOrchestrator:
     def __call__(
         self, _source: str, operation: str, _has_error: bool, payload: INTERSECT_JSON_VALUE
     ) -> IntersectClientCallback:
+
         if _has_error:
             print('============ERROR==============', file=sys.stderr)
             print(operation, file=sys.stderr)
@@ -132,6 +133,7 @@ class ActiveLearningOrchestrator:
             next_payload = DialWorkflowCreationParamsClient(
                 dataset_x=self.dataset_x,
                 dataset_y=self.dataset_y,
+                dim_x=1,
                 bounds=self.bounds,
                 kernel=self.kernel,
                 kernel_args=self.kernel_args,
@@ -182,12 +184,15 @@ class ActiveLearningOrchestrator:
         )
 
     def handle_surrogate_values(self, payload):
+        response_data = payload['data']
         if self.at_grids:
-            self.variance_grid = np.array(payload[1]).reshape((self.meshgrid_size,) * self.num_dims)
-            self.mean_grid = np.array(payload[0]).reshape((self.meshgrid_size,) * self.num_dims)
+            self.variance_grid = np.array(response_data[1]).reshape(
+                (self.meshgrid_size,) * self.num_dims
+            )
+            self.mean_grid = np.array(response_data[0]).reshape((self.meshgrid_size,) * self.num_dims)
         else:
-            self.variance_test = np.array(payload[1])
-            self.mean_test = np.array(payload[0])
+            self.variance_test = np.array(response_data[1])
+            self.mean_test = np.array(response_data[0])
             print(f'Test Mean: {self.mean_test}, Variance: {self.variance_test}')
 
         # end of active learning loop after max_iter
@@ -195,7 +200,7 @@ class ActiveLearningOrchestrator:
             raise IntersectCallbackEnd
 
     def handle_next_points(self, payload):
-        self.x_next = payload
+        self.x_next = payload['data']
         coord_str = ', '.join([f'{coord:.2f}' for coord in self.x_next])
         print(f'Running simulation at ({coord_str}): ', end='', flush=True)
 
