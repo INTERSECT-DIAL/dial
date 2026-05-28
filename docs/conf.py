1ecd74ffe448cdf886ca7a6e432370024ff5b011
Lance-Drane <Lance-Drane@users.noreply.github.com>
2026-05-28 15:36:34 -0400

    add placeholder logo for DIAL website for readthedocs build
    Signed-off-by: Lance-Drane <Lance-Drane@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -61,5 +61,5 @@ html_static_path = ['_static']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'furo'
-# html_logo = '_static/logo.png'
+html_logo = '_static/logo.png'
 html_title = f'INTERSECT-DIAL ({dial_version})'
